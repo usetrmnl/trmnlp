52c92aea7350201d9b20ff4b0fb822ff504d32bc
Rockwell Schrock <rockwell@schrock.me>
2025-04-08 13:28:13 -0400

    Print current API key if already logged in

--- lib/trmnlp/commands/login.rb
+++ lib/trmnlp/commands/login.rb
@@ -4,6 +4,11 @@ module TRMNLP
   module Commands
     class Login < Base
       def call
+        if config.app.logged_in?
+          anonymous_key = config.app.api_key[0..10] + '*' * (config.app.api_key.length - 11)
+          puts "Currently authenticated as: #{anonymous_key}" 
+        end
+
         puts "Please visit #{config.app.account_uri} to grab your API key, then paste it here."
         
         print "API Key: "
