4dd077df0289fa2f7cc2f393b8e69a65485b4cd7
Rockwell Schrock <rockwell@schrock.me>
2025-12-30 09:46:00 -0500

    Support same XML MIME types as TRMNL core

--- lib/trmnlp/context.rb
+++ lib/trmnlp/context.rb
@@ -91,7 +91,7 @@ module TRMNLP
           case content_type
           when 'application/json'
             json = wrap_array(JSON.parse(response.body))
-          when 'application/xml', 'application/soap+xml'
+          when 'text/xml', 'application/xml', 'application/rss+xml', 'application/atom+xml', 'application/soap+xml'
             json = wrap_array(Hash.from_xml(response.body))
           else
             puts "unknown content type received: #{response.headers['content-type']}"
